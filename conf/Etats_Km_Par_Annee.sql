d77414b0d5b636986e6b984e83c691cd37abbb2e
Christian Lorne <christian.lorne@ac-aix-marseille.fr>
2018-10-28 16:38:15 +0100

    Add files via upload

---
+++ conf/Etats_Km_Par_Annee.sql
@@ -0,0 +1,7 @@
+﻿select n.ndf_date, sum(ni.km) from t_ndf n
+join t_ndf_item ni on ni.ndf_id=n.id
+where n.ndf_date >= '2013-01-01' and n.ndf_date < '2014-01-01'
+group by n.ndf_date
+order by n.ndf_date
+
+
